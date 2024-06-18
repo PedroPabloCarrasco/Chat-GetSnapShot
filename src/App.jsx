299036330a2652aba0227a1922ed56ac8ffe795b
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2024-06-18 18:14:10 -0400

    Se agrega avatar junto al nombre
    Se agrega avatar junto al nombre y el mensaje, todos los elementos de agregan de manera aleatorea

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import faker from 'faker'; // Asegúrate de importar faker correctamente
 
+const chatStyle = {
+  width: 230,
+  height: 300,
+  border: '1px solid gray',
+  overflow: 'auto',
+  fontFamily: 'monospace'
+};
 
-//creamos un nuevo componente para renderizar los mensajes
+const messageStyle = {
+  padding: '1em',
+  borderBottom: '1px solid #DDD'
+};
+
+const avatarStyle = {
+  width: 50,
+  height: 50,
+  borderRadius: '50%'
+};
+
+// Creamos un nuevo componente para renderizar los mensajes
 class Chat extends Component {
+  box = React.createRef();
+
+  componentDidUpdate() {
+    this.box.current.scrollTop = this.box.current.scrollHeight;
+  }
+
   render() {
     return (
-      <div>
+      <div style={chatStyle} ref={this.box}>
         {this.props.list.map(item => (
-          <div key={item.id}>{/* Contenido del elemento div */}
-          <p>{item.mensaje}</p>
-          <div>
-            {item.name}
-          </div>
-
-          <img
-          src={item.avatar}
-          alt='Avatar'
-          />
-          
+          <div key={item.id} style={messageStyle}>
+            <p>{item.message}</p>
+            <div>{item.name}</div>
+            <img src={item.avatar} alt='Avatar' style={avatarStyle} />
           </div>
         ))}
       </div>
     );
   }
 }
-
-
-
-
-
 
 class App extends Component {
   state = {
@@ -39,28 +51,23 @@ class App extends Component {
   addMessage = () => {
     // Crear mensaje falso
     const message = {
-      id: faker.random.uuid(),
+      id: faker.datatype.uuid(),
       name: faker.name.findName(),
-      avatar: faker.image.avatar(),
+      avatar: `https://robohash.org/${faker.datatype.uuid()}.png?size=50x50`,
       message: faker.hacker.phrase()
     };
 
     // Agregar a la lista
     this.setState(state => ({
       list: [...state.list, message]
-    }), () => {
-      
-    });
+    }));
   };
 
   render() {
     return (
       <div>
         <h3>Get SnapShot</h3>
-
-        <Chat
-        list={this.state.list}
-        />
+        <Chat list={this.state.list} />
         <button onClick={this.addMessage}>Mostrar mensaje</button>
       </div>
     );
